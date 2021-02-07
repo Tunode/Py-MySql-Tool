8ed0abffd33a5213c5617fefc3e0de7df944c5e2
Tunode <tunodemg@gmail.com>
2021-02-07 21:00:40 +0200

    removed ask db info and replaced function with input variables

--- mysqltool1.py
+++ mysqltool1.py
@@ -2,31 +2,19 @@ import mysql.connector
 
 class mysql_tool:
 
-	def ask_db_info(self):#Asks loggin details form user with: INPUT(CMD) -> Done
-		host_ip = input("Host ip: ")
-		db_user = input("DB Username: ")
-		db_passwd = input("DB Password: ")
-		db_name = input("DB_Name: ")
-		self.db_info = {
-			"host_ip": host_ip,
-			"db_user": db_user,
-			"db_passwd": db_passwd,
-			"dbname": db_name
-		}
-
-	def db_connector_new(self):#Connects mysql without DB name: (this is for gen_db functionality) -> done
+	def db_connector_new(self, host_ip, db_username, db_passwd):#Connects mysql without DB name: (this is for gen_db functionality) -> done
 		self.DB = mysql.connector.connect(
-			host = self.db_info["host_ip"],
-			user = self.db_info["db_user"],
-			passwd = self.db_info["db_passwd"]
+			host = host_ip,
+			user = db_username,
+			passwd = db_passwd
 			)
 
-	def db_connector(self):#Connetcs with excisting DB -> Done
+	def db_connector(self, host_ip, db_username, db_passwd, db_name):#Connetcs with excisting DB -> Done
 		self.DB = mysql.connector.connect(
-			host = self.db_info["host_ip"],
-			user = self.db_info["db_user"],
-			passwd = self.db_info["db_passwd"],
-			database = self.db_info["dbname"]
+			host = host_ip,
+			user = db_username,
+			passwd = db_passwd,
+			database = db_name
 			)
 
 	def close_connection(self):#Simply closes DB Connection -> Done
@@ -76,6 +64,8 @@ class mysql_tool:
 		self.db_connector()
 		DBCursor = self.DB.cursor()
 		DBCursor.execute("ALTER TABLE "+table_name+ " DROP COLUMN "+column_name)
-		self.close_connection()
+		self.close_connection()#Simple MYSQL module to make daily MYSQL Tasks easier.
 
+#Testing area:
 DBTool = mysql_tool()
+#DBTool.gen_db()
