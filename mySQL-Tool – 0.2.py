8c184f79d583d01982a1d8a40c2d28e1a2afb0a2
Tunode <tunodemg@gmail.com>
2021-02-07 13:27:38 +0200

    versio update 0.5 -> 80% functions working

--- mySQL-Tool – 0.2.py
+++ mySQL-Tool – 0.2.py
@@ -32,10 +32,10 @@ class mysql_tool:
 	def close_connection(self):#Simply closes DB Connection -> Done
 		self.DB.close()
 
-	def gen_db(self): #Create Database -> Done
+	def gen_db(self, dbname): #Create Database -> Done
 		self.db_connector_new()
 		DBCursor = self.DB.cursor()
-		DBCursor.execute("CREATE DATABASE "+ self.db_info["dbname"])
+		DBCursor.execute("CREATE DATABASE "+dbname)
 		self.close_connection()
 
 	def gen_table(self, tablename): #Create Table with DataID column -> Done
@@ -44,19 +44,38 @@ class mysql_tool:
 		DBCursor.execute("CREATE TABLE IF NOT EXISTS "+ tablename +"(dataID int PRIMARY KEY AUTO_INCREMENT)")
 		self.close_connection()
 
-	def add_column(self): #Add Column in selected exsisting Table.
+	def add_column(self, table_name, column_name): #Add Column in selected exsisting Table. -> Done
 		self.db_connector()
 		DBCursor = self.DB.cursor()
-		table_name = "data1"
-		column_name = "username"
-		column_type = "VARCHAR(255)"
 		#DBCursor.execute("CREATE TABLE users (name varchar(255)))")
-		DBCursor.execute("CREATE TABLE " + table_name + "(" + column_name + " VARCHAR(255))")
+		DBCursor.execute("ALTER TABLE " + " " + table_name + " ADD " "(" + column_name + " VARCHAR(255))")
+		self.close_connection()
+
+	def show_databases(self): #Prints list of databases in MYSQL server. -> Working.
+		self.db_connector()
+		DBCursor = self.DB.cursor()
+		DBCursor.execute("SHOW DATABASES")
+		l = DBCursor.fetchall()
+		l = [ i[0] for i in l ]
+		print(l)
+		self.close_connection()
+
+	def delete_db(self, database_name): #Simply removes DB from MYSQL server -> Done
+		self.db_connector()
+		DBCursor = self.DB.cursor()
+		DBCursor.execute("DROP DATABASE "+database_name)
+		self.close_connection()
+
+	def delete_table(self, table_name): #Delete's table from DB you wish. -> Done
+		self.db_connector()
+		DBCursor = self.DB.cursor()
+		DBCursor.execute("DROP TABLE "+table_name)
+		self.close_connection()
+
+	def delete_column(self, table_name, column_name): #Delete column from Table you name in DB of your wish. -> Done
+		self.db_connector()
+		DBCursor = self.DB.cursor()
+		DBCursor.execute("ALTER TABLE "+table_name+ " DROP COLUMN "+column_name)
 		self.close_connection()
 
 DBTool = mysql_tool()
-
-DBTool.ask_db_info()
-#DBTool.gen_db()
-#DBTool.gen_table("user_data")
-DBTool.add_column()
