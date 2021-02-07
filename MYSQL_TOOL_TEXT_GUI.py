9e966693cd56bd2c4120db2bd934902ad3f93d9b
Tunode <tunodemg@gmail.com>
2021-02-07 20:58:38 +0200

    added 2. function and updated import fomation + started to build the app it self (now is-local cmd works)

--- MYSQL_TOOL_TEXT_GUI.py
+++ MYSQL_TOOL_TEXT_GUI.py
@@ -1,13 +1,22 @@
 #MYSQL_TOOL_TEXT_GUI.py
-import mysqltool1
-DBtool = mysqltool1.mysql_tool()
-
-class textgui:
-	def ask_if_local(self):
-		print("MSQL Server is local?")
-		local=input("y or n?: ")
+from mysqltool1 import mysql_tool
+db_tool = mysql_tool()
+#--------------------------------
+class textgui: #OOP of text gui:
+	def ask_if_local(self): #Ask from user if MYSQL server is hosted localy or in spesific IP.
+		print("-"*20)
+		print("- MSQL Server is local? -")
+		local=input("-| y or n?: ")
 		if local == "y" or "Y":
 			mysql_ip = "localhost"
 		else:
 			mysql_ip = input("MSQL Server IP?: ")
-		return mysql_i#Ask from user if MYSQL server is hosted localy or in spesific IP.
+		return mysql_ip
+
+	def ask_mysql_info(self):
+		pass
+
+# The Code it self:
+mstg = textgui()
+#------------------
+mstg.ask_if_local()
