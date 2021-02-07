759d8fbed1069df7dda1b2905356e692c0be186e
Tunode <tunodemg@gmail.com>
2021-02-07 13:59:37 +0200

    Created first Function, -> Asks i Hosted localy? (MYSQL)

--- MYSQL_TOOL_TEXT_GUI.py
+++ MYSQL_TOOL_TEXT_GUI.py
@@ -1,6 +1,13 @@
 #MYSQL_TOOL_TEXT_GUI.py
 import mysqltool1
-
 DBtool = mysqltool1.mysql_tool()
 
-DBtool.ask_db_info()
+class textgui:
+	def ask_if_local(self):
+		print("MSQL Server is local?")
+		local=input("y or n?: ")
+		if local == "y" or "Y":
+			mysql_ip = "localhost"
+		else:
+			mysql_ip = input("MSQL Server IP?: ")
+		return mysql_i#Ask from user if MYSQL server is hosted localy or in spesific IP.
