3dfa270d15610563c26f69afd957565a2de4b02e
Tunode <tunodemg@gmail.com>
2021-02-14 15:13:36 +0200

    litle update for guiv.2

--- mysql-gui-2.py
+++ mysql-gui-2.py
@@ -2,11 +2,12 @@ import os
 
 class gui:
 
-	def os_fix(self):
+	def os_fix(self): #Changes terminal color to green, Changes terminal title + Clear's Terminal.
+		os.system("color 2")
 		os.system("title MYSQL_TOOL_0.2")
 		os.system('clear')
 
-	def Main_Menu(self):
+	def main_menu(self): #Main menu.
 		self.os_fix()
 		print("-- MYSQL_TOOL_0.2 --")
 		print("")
@@ -18,38 +19,150 @@ class gui:
 		print("")
 		mm_select = input("- Valinta - |")
 		self.os_fix()
-		return mm_select
+		return int(mm_select)
 
-	def DB_Menu(self):
+	def ask_mysql_server(self): #Ask mysql server loggin details.
+		self.os_fix()
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.hostip = input("MySQL_IP: ")
+		self.username = input("MySQL_Ùsername: ")
+		self.passwd = input("MySQL_Password: ")
+		#ADD Functionality here.
+		self.os_fix()
+		
+	def ask_mysql_server_and_db_name(self): #Ask mysql server loggin details + db name.
+		self.os_fix()
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.hostip = input("MySQL_IP: ")
+		self.username = input("MySQL_Ùsername: ")
+		self.passwd = input("MySQL_Password: ")
+		self.db_name = input("MySQL_DB_Name: ")
+		#ADD Functionality here.
+		self.os_fix()
+
+	def db_menu(self): #Altter of close DB Menu.
 		self.os_fix()
 		print("-- MYSQL_TOOL_0.2 --")
 		print("")
 		print("- 1 -| Altter existing DB? ")
-		print("- 2 -| Close ")
-		print("")
-		mm_select = input("- Valinta - |")
-		self.os_fix()
-		return mm_select
-
-	def Alter_DB(self):
-		self.os_fix()
-		print("-- MYSQL_TOOL_0.2 --")
-		print("")
-		print("- 1 -| Add table ")
-		print("- 2 -| Add Column ")
-		print("- 3 -| Remove Table ")
-		print("- 4 -| Remove Column ")
+		print("- 2 -| DB List ")
+		print("- 3 -| Remove DB ")
+		print("- 4 -| Create new DB ")
 		print("- 5 -| Close ")
 		print("")
 		mm_select = input("- Valinta - |")
 		self.os_fix()
-		return mm_select
+		return int(mm_select)
 
+	def alter_db(self): #Show's all DB alltering settings.
+		self.os_fix()
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.n_db_name = input("New database name: ")
+		#ADD Functionality here.
+		self.os_fix()
 
+	def db_list(self):
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		#ADD Functionality here.
+		print("DB List:"+db_list)
+		self.os_fix()
+
+	def remove_db(self):
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.remove_db_name = input("Name of database to remove: ")
+		#ADD Functionality here.
+		self.os_fix()
+
+	def add_table(self):
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.add_table_name = input("Name of table to add: ")
+		#ADD Functionality here.
+		self.os_fix()
+
+	def add_column(self):
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.add_column_name = input("Name of column to add: ")
+		#ADD Functionality here.
+		self.os_fix()
+
+	def remove_table(self):
+		self.os_fix()
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.remove_column_name = input("Name of table to remove: ")
+		#ADD Functionality here.
+		self.os_fix()
+
+	def remove_column(self):
+		self.os_fix()
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		self.remove_column_name = input("Name of column to remove: ")
+		#ADD Functionality here.
+		self.os_fix()
+
+	def new_db_name(self):
+		self.os_fix()
+		print("-- MYSQL_TOOL_0.2 --")
+		print("")
+		print("- 1 -| Altter existing DB? ")
+		print("- 2 -| DB List ")
+		print("- 3 -| Remove DB ")
+		print("- 4 -| Create new DB ")
+		print("- 5 -| Close ")
+		print("")
+		mm_select = input("- Valinta - |")
+		self.os_fix()
+		return int(mm_select)
 
 #Code:
 gui=gui()
 
-gui.Main_Menu()
-gui.DB_Menu()
-gui.Alter_DB()
+while True:
+	mm_select = gui.main_menu()
+	if mm_select == 1: #Connect to existing DB.
+		gui.ask_mysql_server_and_db_name()
+		while True:
+			dbm_select = gui.db_menu()
+			if dbm_select == 1: #Alter DB
+				while True:
+					adb_select = gui.alter_db()
+					if adb_select == 1: #Add table.
+						gui.add_table()
+					if adb_select == 2: #Add column.
+						gui.add_table()
+					if adb_select == 3: #Remove table.
+						gui.remove_table()
+					if adb_select == 4: #Remove column.
+						gui.remove_column()
+					if adb_select == 5: #Close alter db.
+						break
+			if dbm_select == 2: #DB list.
+				gui.db_list()
+			if dbm_select == 3: #Remove DB.
+				gui.remove_db()
+			if dbm_select == 4: #Create new DB_Menu.
+				gui.new_db_name()
+			if dbm_select == 5: #Close DB_Menu.
+				break
+	if mm_select == 2: #Create new DB.
+		gui.ask_mysql_server()
+		gui.new_db_name()
+		#in progress (infite loop)
+	if mm_select == 3: #DB List.
+		gui.db_list()
+	if mm_select == 4: #Remove DB.
+		gui.remove_db()
+	if mm_select == 5: #Close application.
+		break
+
+
+
+
