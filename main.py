5ffa19892017251b8daacc39eabbcef09ca6fc3f
Tunode <tunodemg@gmail.com>
2021-03-27 23:32:04 +0200

    full rework for UI

--- main.py
+++ main.py
@@ -48,17 +48,17 @@ def change_logging_details(new_ip, new_sql_username, new_sql_password, new_sql_d
 	logging_details.update({"sql_database": f"{new_sql_database}"})
 	save_(logging_details)
 
-def multiple_DB_slection():
-	db_list = sql.show_databases()
+def multiple_DB_slection(db_list):
 	if len(db_list) > 1:
-		M_DB_Menu_Selection = gui.multiple_DB_menu()
+		M_DB_Menu_Selection = gui.multiple_DB_menu(db_list)
 		logging_details = load_loggin_details()
 		logging_details.update({"sql_database": f"{M_DB_Menu_Selection}"})
+		save_loggin_details(logging_details)
+		return logging_details
 	else:
 		pass
 
-
-gui = gui("0.8.9")
+gui = gui("0.9.2")
 sql = mysql_tool()
 
 #----
@@ -86,119 +86,104 @@ try: #Create settings.jason and add default settings.(if not created allready.)
 except:
 	pass
 
-while mm == True:
-	password = gui.ask_loggin_details()
-	if password == "tunode":
+password = gui.ask_loggin_details()
+if password == "tuno":
+	while mm == True:
 		mm_select = gui.main_menu()
-		if mm_select == "1": #Connect to existing DB.
-			try:
-				if check_setting_status("1") == "off":
-					log_details = gui.ask_mysql_server_and_db_name()
-					save_loggin_details(log_details)
-					sql.db_connector(log_details)
-					db_m = True
-				elif check_setting_status("1") == "on":
-					sql.db_connector(load_loggin_details())
-					db_m = True
-			except mysql.connector.Error as err:
-				gui.message(f"ERROR: {err}")
-				db_m = False
-			while db_m == True:
-				dbm_select = gui.db_menu()
-				if dbm_select == "1": #Alter DB
-					alt_db = True
-					while alt_db == True:
-						adb_select = gui.alter_db()
-						if adb_select == "1": #Add table.
-							try:
-								sql.gen_table(gui.add_table())
-							except:
-								gui.message("ERROR: Incorrect input.")
-						if adb_select == "2": #Add column.
-							try:
-								sql.add_column(gui.add_column())
-							except:
-								gui.message("ERROR: Incorrect input.")
-						if adb_select == "3": #Remove table.
-							try:
-								sql.delete_table(gui.remove_table())
-							except:
-								gui.message("ERROR: Incorrect input.")
-						if adb_select == "4": #Remove column.
-							try:
-								sql.delete_column(gui.remove_column())
-							except:
-								gui.message("ERROR: Incorrect input")
-						if adb_select == "5": #Close alter db.
-							alt_db = False
-				if dbm_select == "2": #DB list.
-					try:
-						gui.db_list(sql.show_databases())
-					except:
-						gui.message("ERROR: - - - - - - - - - - - ")
-				if dbm_select == "3": #Remove DB.
-					try:
-						sql.delete_db(gui.remove_db())
-					except:
-						gui.message("ERROR: Incorrect input.")
-				if dbm_select == "4": #Create new DB_Menu.
-					try:
-						sql.gen_db(gui.new_db_name())
-					except:
-						gui.message("ERROR: Incorrect input.")
-				if dbm_select == "5": #Close DB_Menu.
-					db_m = False
-		if mm_select == "2": #Create new DB.
-			try:
-				if check_setting_status("1") == "off":
-					log_details = gui.ask_mysql_server()
-					save_loggin_details(log_details)
-					sql.db_connector_new(log_details)
-					sql.gen_db(gui.new_db_name())
-				elif check_setting_status("1") == "on":
-					sql.db_connector_new(load_loggin_details())
-					sql.gen_db(gui.new_db_name())
-			except:
-				gui.message("ERROR: Incorrect logging details.")
-		if mm_select == "3": #Open Settings menu.
-			close_sm = True
-			while close_sm:
-				sm_select = gui.settings_menu() #Runs: SettingsMenu UI
-				if sm_select == "1":#Remember last session. (Mysql Server loggin deails)
-					change_setting(sm_select)
-				elif sm_select == "2":#Confim if wish to leave, before close menu.
-					change_setting(sm_select)
-				elif sm_select == "3":#Change color for app ui.
-					change_setting(sm_select)
-				elif sm_select == "4":#Opens an advanced options menu.
-					change_setting(sm_select)
-				elif sm_select == "5":#Close SettingsMenu
-					if check_setting_status("2") == "on":
-						close_settings = gui.ask_if_close("Settings")
-						if close_settings == "y" or close_settings == "Y" or close_settings == "yes":
-							close_sm = False
-						else:
-							close_sm = True
-					else:
-						close_sm = False
-		if mm_select == "4": #Close application.
-			if check_setting_status("2") == "off":
+		if mm_select == "1": #Connect to MySql Server.
 				try:
-					sql.close_connection()
-					mm = False
-				except:
-					mm = False
-			elif check_setting_status("2") == "on":
-				close_app = gui.ask_if_close("App")
-				if close_app == "y" or close_app == "Y" or close_app == "yes":
+					if check_setting_status("1") == "off":
+						log_details = gui.ask_mysql_server()
+						save_loggin_details(log_details)
+						sql.db_connector(log_details)
+						db_m = True
+					elif check_setting_status("1") == "on":
+						sql.db_connector(load_loggin_details())
+						db_m = True
+				except mysql.connector.Error as err:
+					gui.message(f"ERROR: {err}")
+					db_m = False
+				while db_m == True:
+					dbm_select = gui.db_menu()
+					if dbm_select == "1": #Open menu to Select database and then opens menu to altter selected database.						
+						sql.db_connector(multiple_DB_slection(sql.show_databases()))
+						alt_db = True
+						while alt_db == True:
+							adb_select = gui.alter_db()
+							if adb_select == "1": #Add table.
+								try:
+									sql.gen_table(gui.add_table())
+								except:
+									gui.message("ERROR: Incorrect input.")
+							if adb_select == "2": #Add column.
+								try:
+									sql.add_column(gui.add_column())
+								except:
+									gui.message("ERROR: Incorrect input.")
+							if adb_select == "3": #Remove table.
+								try:
+									sql.delete_table(gui.remove_table())
+								except:
+									gui.message("ERROR: Incorrect input.")
+							if adb_select == "4": #Remove column.
+								try:
+									sql.delete_column(gui.remove_column())
+								except:
+									gui.message("ERROR: Incorrect input")
+							if adb_select == "5": #Close alter db.
+								alt_db = False
+					if dbm_select == "2": #Remove DB.
+						try:
+							sql.delete_db(gui.remove_db(sql.show_databases()))
+						except:
+							gui.message("ERROR: Incorrect input.")
+					if dbm_select == "3": #Create new DB_Menu.
+						try:
+							sql.gen_db(gui.new_db_name())
+						except:
+							gui.message("ERROR: Incorrect input.")
+					if dbm_select == "4": #Close DB_Menu.
+						db_m = False
+		if mm_select == "2": #Open Settings menu.
+				close_sm = True
+				while close_sm:
+					sm_select = gui.settings_menu() #Runs: SettingsMenu UI
+					if sm_select == "1":#Remember last session. (Mysql Server loggin deails)
+						change_setting(sm_select)
+					elif sm_select == "2":#Confim if wish to leave, before close menu.
+						change_setting(sm_select)
+					elif sm_select == "3":#Change color for app ui.
+						change_setting(sm_select)
+					elif sm_select == "4":#Opens an advanced options menu.
+						change_setting(sm_select)
+					elif sm_select == "5":#Close SettingsMenu
+						if check_setting_status("2") == "on":
+							close_settings = gui.ask_if_close("Settings")
+							if close_settings == "y" or close_settings == "Y" or close_settings == "yes":
+								close_sm = False
+							else:
+								close_sm = True
+						else:
+							close_sm = False
+		if mm_select == "3": #Close application.
+				if check_setting_status("2") == "off":
 					try:
 						sql.close_connection()
 						mm = False
 					except:
 						mm = False
-				else:
-					mm = True
+				elif check_setting_status("2") == "on":
+					close_app = gui.ask_if_close("App")
+					if close_app == "y" or close_app == "Y" or close_app == "yes":
+						try:
+							sql.close_connection()
+							mm = False
+						except:
+							mm = False
+					else:
+						mm = True
 		else:
 			pass
-	else:
-		mm = False
+else:
+	gui.message("Incorrect Password.")
+
--- mysqltool1_gui.py
+++ mysqltool1_gui.py
@@ -14,11 +14,10 @@ class gui:
 		self.os_fix()
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		password = getpass(prompt="- Password -:")
+		password = getpass(prompt="-| Password: ")
 		print("")
 		self.os_fix()
 		return password
-
 
 	def os_fix(self): #Changes terminal color to green, Changes terminal title + Clear's Terminal.
 		os.system("color 2")
@@ -29,12 +28,11 @@ class gui:
 		self.os_fix()
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		print("- 1 -| Connect to existing DB ")
-		print("- 2 -| Create new DB ")
-		print("- 3 -| Settings ")
-		print("- 4 -| Close ")
+		print("- 1 -| Connect to MySQL server ")
+		print("- 2 -| Settings ")
+		print("- 3 -| Close ")
 		print("")
-		mm_select = input("- Valinta - |")
+		mm_select = input("-| Select: ")
 		self.os_fix()
 		return mm_select
 
@@ -58,39 +56,21 @@ class gui:
 			"sql_ip": hostip,
 			"sql_username": username,
 			"sql_password": passwd,
-			"sql_database": "db_name"
+			"sql_database": "db"
 		}
 		self.os_fix()
 		return mysql_loggin_data
 		
-	def ask_mysql_server_and_db_name(self): #Ask mysql server loggin details + db name to connect.
-		self.os_fix()
-		print(f"-- MYSQL_TOOL_{self.version} --")
-		print("")
-		hostip = input("MySQL_IP: ")
-		username = input("MySQL_Ùsername: ")
-		passwd = input("MySQL_Password: ")
-		db_name = input("MySQL_DB_Name: ")
-		mysql_loggin_data ={
-			"sql_ip": hostip,
-			"sql_username": username,
-			"sql_password": passwd,
-			"sql_database": db_name
-		}
-		self.os_fix()
-		return mysql_loggin_data
-
 	def db_menu(self): # Prints submenu when connected to database.
 		self.os_fix()
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		print("- 1 -| Altter existing DB? ")
-		print("- 2 -| DB List ")
-		print("- 3 -| Remove DB ")
-		print("- 4 -| Create new DB ")
-		print("- 5 -| Close ")
+		print("- 1 -| Select database to altter. ")
+		print("- 2 -| Remove database ")
+		print("- 3 -| Create new database ")
+		print("- 4 -| Close ")
 		print("")
-		mm_select = input("- Valinta - |")
+		mm_select = input("-| Select: ")
 		self.os_fix()
 		return mm_select
 
@@ -119,18 +99,23 @@ class gui:
 		print(" [] = OFF | [X] = ON ")
 		print("")
 		print(f"- 1 -| Remember last session: {option_status(setting_1)} ")
-		print(f"- 2 -| Confirm before leave menu: {option_status(setting_2)} ")
-		print(f"- 3 -| Change Color: {option_status(setting_3)} ")
-		print(f"- 4 -| Advanced Mode: {option_status(setting_4)} ")
+		print(f"- 2 -| Confirm before leave current menu: {option_status(setting_2)} ")
+		print(f"- 3 -| Change color: {option_status(setting_3)} ")
+		print(f"- 4 -| Advanced mode: {option_status(setting_4)} ")
 		print(f"- 5 -| Close ")
 		print("")
-		mm_select = input("- Valinta - |")
+		mm_select = input("-| Select: ")
 		self.os_fix()
 		return mm_select
 
 	def alter_db(self): #Show's all DB alltering settings.
+
+		loggin_details = json.load(open("logging_details.json"))
+		current_database_name = loggin_details["sql_database"]	
 		self.os_fix()
 		print(f"-- MYSQL_TOOL_{self.version} --")
+		print("")
+		print(f"- Current database: {current_database_name}")
 		print("")
 		print("- 1 -| Add table ")
 		print("- 2 -| Add Column ")
@@ -138,11 +123,13 @@ class gui:
 		print("- 4 -| Remove Column ")
 		print("- 5 -| Close ")
 		print("")
-		mm_select = input("- Valinta - |")
+		mm_select = input("-| Select: ")
 		self.os_fix()
 		return mm_select
 
 	def multiple_DB_menu(self, db_list): #Check's if MySQL server have multiple DB's.
+		print(f"-- MYSQL_TOOL_{self.version} --")
+		print("")
 		print(f"-| Select DB |-")
 		print("")
 		x = 0
@@ -150,40 +137,40 @@ class gui:
 			print(f"- {idx} -|{db[x]}")
 			x + 1
 		print("")
-		menu_selection= int(input("Choice: ")) - 1
-		return db_list[menu_selection]
+		menu_selection= int(input("-| Select: ")) - 1
+		x = db_list[menu_selection]  
+		y = len(str(x))
+		z = y - 3
+		return str(x)[2:z]
 
-	def db_list(self, db_list):#Prints text wiew for lists of databases in mysql server.
+	def remove_db(self, db_list): #Ask from user what is name for database to remove.
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
 		print("-| DB List: ")
-		print("------------")
+		print("")
 		for db in db_list:
 			x = 0
 			print(f"-|{db[x]}")
-			x + 1 
-		time.sleep(5)
-		self.os_fix()
-
-	def remove_db(self): #Ask from user what is name for database to remove.
-		print(f"-- MYSQL_TOOL_{self.version} --")
+			x + 1
 		print("")
-		self.remove_db_name = input("Name of database to remove: ")
+		print("-| Cancel with: c ")
+		print("")
+		self.remove_db_name = input("-| Name of database to remove: ")
 		return self.remove_db_name
 		self.os_fix()
 
 	def add_table(self): #Asks from user what name wanted to new table.
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		self.add_table_name = input("Name of table to add: ")
+		self.add_table_name = input("-| Name of table to add: ")
 		return self.add_table_name
 		self.os_fix()
 
 	def add_column(self): #Asks from user what name wanted to new column.
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		add_column_name = input("Name of column to add: ")
-		table_to_conn = input("Name of table to add column: ")
+		add_column_name = input("-| Name of column to add: ")
+		table_to_conn = input("-| Name of table to add column: ")
 		add_column_data ={
 			"sql_new_column": add_column_name,
 			"sql_table_to_conn": table_to_conn
@@ -195,7 +182,7 @@ class gui:
 		self.os_fix()
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		self.remove_table_name = input("Name of table to remove: ")
+		self.remove_table_name = input("-| Name of table to remove: ")
 		self.os_fix()
 		return self.remove_table_name
 
@@ -203,7 +190,7 @@ class gui:
 		self.os_fix()
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		self.remove_column_name = input("Name of column to remove: ")
+		self.remove_column_name = input("-| Name of column to remove: ")
 		self.os_fix()
 		return self.remove_column_name
 
@@ -211,7 +198,7 @@ class gui:
 		self.os_fix()
 		print(f"-- MYSQL_TOOL_{self.version} --")
 		print("")
-		add_database_name = input("Name of database to Create: ")
+		add_database_name = input("-| Name of database to create: ")
 		self.os_fix()
 		return add_database_name
 
@@ -225,9 +212,10 @@ class gui:
 		return ask_if_close
 
 
-
+'''
 #Test Area:
 sql = mysql_tool()
 ui = gui("0.9.1.2")
-sql.db_connector(ui.ask_mysql_server_and_db_name())
+sql.db_connector(ui.ask_mysql_server())
 print(ui.multiple_DB_menu(sql.show_databases()))
+'''
